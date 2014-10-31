349275e3a8814ad6798e8648d6d584249c9a659e
Jelmer Timmer <jp.timmer.cxiii@gmail.com>
2014-10-31 16:29:24 +0100

    added form validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,8 +1,8 @@
 class Reservation < ActiveRecord::Base
-	##validates :first_name, :last_name, presence: true,
-	##						{ minimum: 1 }
-	##validates :phone, numericality: true, presence: true,
-	##						{ minimum: 7 }
-	##validates :email, presence: true, with: /@/
-
+	validates :first_name, :last_name, presence: true, 
+										length: { minimum: 1, maximum: 20 }
+	validates :phone, numericality: true, presence: true,
+										length: { minimum: 7 }
+	validates_format_of :email, presence: true, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+	validates :date, :start, :end, presence: true
 end
