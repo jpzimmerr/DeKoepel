ad708f6892f47440968eb0cf9867edf47de13ec0
Jelmer Timmer <jp.timmer.cxiii@gmail.com>
2014-11-18 10:59:20 +0100

    got the mailer working

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,8 +1,16 @@
 class ReservationMailer < ActionMailer::Base
   default from: "from@example.com"
 
-  def confirmation_email(name)
-  	@reservation.customer.last_name = name
-  	mail(to: @reservation.customer.email, subject: 'Bevestiging van uw reservering')
+  def confirmation_email(reservation)
+  	@last_name = reservation.customer.last_name
+  	@email = reservation.customer.email
+  	@first_name = reservation.customer.first_name
+  	@phone = reservation.customer.phone
+  	@company = reservation.customer.company.name
+  	@date = reservation.date
+  	@start = reservation.start
+  	@end = reservation.end
+  	@description = reservation.description
+  	mail(to: @email, subject: 'Bevestiging van uw reservering')
   end
 end
