339062912b10b483c7c4b9892c29a44b97f8c461
Jelmer Timmer <jp.timmer.cxiii@gmail.com>
2014-11-18 11:47:25 +0100

    added error handling
    Needs to be properly translated

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,10 @@ class Customer < ActiveRecord::Base
 
 	belongs_to :company
 	accepts_nested_attributes_for :company
+
+	validates :first_name, :last_name, presence: true, 
+										length: { minimum: 1, maximum: 20 }
+	validates :phone, numericality: true, presence: true,
+										length: { minimum: 7 }
+	validates_format_of :email, presence: true, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,6 +2,8 @@ class Reservation < ActiveRecord::Base
 	belongs_to :customer
 	accepts_nested_attributes_for :customer
 
+	validates :date, :start, :end, presence: true
+
 	def self.search(query)
 		where("date like ?", "%#{query}%")
 	end
