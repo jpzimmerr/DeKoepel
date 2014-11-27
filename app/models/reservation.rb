c48fffae6fc3cb81de3486f12a5f3941e6220c8a
Jelmer Timmer <jp.timmer.cxiii@gmail.com>
2014-11-27 14:46:53 +0100

    added firstname search params

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,6 +6,6 @@ class Reservation < ActiveRecord::Base
 
 	def self.search(query)
 		q = "%#{query}%"
-		joins(customer: :company).where('date::text like ? or upper(customers.last_name) like ? or upper(companies.name) like ?', q, q.upcase, q.upcase)
+		joins(customer: :company).where('date::text like ? or upper(customers.last_name) like ? or upper(customers.first_name) like ? or upper(companies.name) like ?', q, q.upcase, q.upcase, q.upcase)
 	end
 end	
