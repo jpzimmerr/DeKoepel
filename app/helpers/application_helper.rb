15d5bb929899eadac15f4b5e563b2d2f08a591b1
Jelmer Timmer <jp.timmer.cxiii@gmail.com>
2014-11-27 15:39:37 +0100

    sorting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 	def sortable(column, title = nil)
         title ||= column.titleize
-        css_class = column == sort_column ? "current #{sort_direction}" : nil
-        direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-        link_to "#{title} <i class='#{direction == "desc" ? "icon-chevron-down" : "icon-chevron-up"}'></i>".html_safe, {:sort => column, :direction => direction}, {:class => css_class}
+        css_class = (column == sort_column) ? "current #{sort_direction}" : nil
+        direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc" 
+        link_to "#{title} #{direction} <i class='glyphicon #{direction == "desc" ? "glyphicon-chevron-down" : "glyphicon-chevron-up"}'></i>".html_safe, {:sort => column, :direction => direction}, {:class => css_class}
 	end
 end
