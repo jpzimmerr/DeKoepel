3f96d75c8e8b055b44657070ed731ed663c85b70
Jelmer Timmer <jp.timmer.cxiii@gmail.com>
2014-11-27 11:42:22 +0100

    yalla

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -7,6 +7,12 @@ class Reservation < ActiveRecord::Base
 	#q = "%#{query}%"
 	#Reservation.joins(:customer).where('customers.last_name = ?', q)
 
+	#<<-SQL
+	#	SELECT reservations.* FROM reservations LEFT OUTER JOIN customers ON customer.last_name = last_name
+	#	SELECT reservations.* FROM reservations LEFT OUTER JOIN companies ON company.name = name
+	#	SELECT reservations.* FROM reservations ON date = date
+	#SQL
+
 	def self.search(query)
 		where('date like ? OR last_name like ? OR name like ?', query, query, query)
 	end
