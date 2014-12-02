82a7ef54b90f0aaa6a1914c018f16d937270525b
Jelmer Timmer <jp.timmer.cxiii@gmail.com>
2014-12-02 22:09:31 +0100

    fixing migrations

--- db/migrate/20141202204938_add_colums_to_reservations.rb
+++ db/migrate/20141202204938_add_colums_to_reservations.rb
@@ -1,8 +1,8 @@
 class AddColumsToReservations < ActiveRecord::Migration
   def change
 
-    add_column :reservations, :started_at, :time
-    add_column :reservations, :ended_at, :time
+    add_column :reservations, :started_at, :datetime
+    add_column :reservations, :ended_at, :datetime
   	
   end
 end
