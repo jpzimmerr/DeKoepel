c77725d074d905a5e9dd4019cf455f3c24b77f1e
Jelmer Timmer <jp.timmer.cxiii@gmail.com>
2014-12-10 15:37:30 +0100

    getting the mailer right up in there

--- app/mailers/sites_mailer.rb
+++ app/mailers/sites_mailer.rb
@@ -1,3 +1,16 @@
 class SitesMailer < ActionMailer::Base
   default from: "from@example.com"
+
+  def confirmation_email(reservation)
+  	@last_name = reservation.customer.last_name
+  	@email = reservation.customer.email
+  	@first_name = reservation.customer.first_name
+  	@phone = reservation.customer.phone
+  	@company = reservation.customer.company.name
+  	@date = reservation.date
+  	@start = reservation.start
+  	@end = reservation.end
+  	@description = reservation.description
+  	mail(to: @email, subject: 'Bevestiging van uw reservering')
+  end
 end
