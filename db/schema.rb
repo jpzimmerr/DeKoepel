df9a019dc0f27d7e3f75129eb2b81d0934219451
Retsilloh <ptran058@gmail.com>
2014-12-16 13:04:19 +0100

    Rake db:migrate add column locations to reservation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141211132730) do
+ActiveRecord::Schema.define(version: 20141216120146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20141211132730) do
     t.text     "note"
     t.datetime "started_at"
     t.datetime "ended_at"
+    t.string   "location"
   end
 
 end
