261513a9d72ad58f6e73678642d3861fc59359a2
annestoer <stoeranne@gmail.com>
2015-01-07 12:05:19 +0100

    create arrangement

--- app/controllers/arrangements_controller.rb
+++ app/controllers/arrangements_controller.rb
@@ -1,11 +1,28 @@
 class ArrangementsController < ApplicationController
 
 	def index
-		@arrangements = Page.all
+		@arrangements = Page.where(:kind => "2").order('created_at DESC')
 	end
 
 	def edit
 		@arrangement = Page.find(params[:id])
 	end
+
+	def new
+		@arrangement = Page.new
+		@arrangement.title = 'Dit is een titel'
+	end
+
+	def create
+		arrangement = Page.new
+		arrangement = Page.new(arrangement_params)
+		arrangement.save
+		redirect_to arrangements_path
+	end
+
+	private
+		def arrangement_params
+			params.require(:arrangement).permit(:title,:body,:kind)	
+		end
 
 end
