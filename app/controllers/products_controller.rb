d3d40bacfad4647293dc2cfcc7ca4fb70beb7876
mandeeeh <mipodmy@gmail.com>
2015-01-07 15:26:11 +0100

    create products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,17 @@ class ProductsController < ApplicationController
 	def index
 		@products = Product.all
 	end
+	def new
+		@product = Product.new
+	end
+	def create
+		product = Product.new
+		product = Product.new(product_params)
+		product.save
+		redirect_to products_path
+	end
+	private
+		def product_params
+			params.require(:product).permit(:name,:price,:per)
+		end
 end
